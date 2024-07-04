53ad8696a17027818dd212f29ae944218080af5c
Muregz <fabian.murega@student.moringaschool.com>
2024-07-04 10:03:55 +0300

    master

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1; 
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+let numberSet = [5, 12, 20, 8, 15];
+let max = Math.max(...numberSet);
 
-// Write your code here
